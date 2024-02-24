06b694e0b1760f438b17cf61246f34818e94595e
Parth Passi <parthpassi@gmail.com>
2024-02-24 16:32:59 +0530

    Added caption settings

--- app/src/main/java/com/example/voiceversa/Center.java
+++ app/src/main/java/com/example/voiceversa/Center.java
@@ -2,14 +2,17 @@ package com.example.voiceversa;
 
 
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,10 +22,11 @@ import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class Center extends Fragment {
     int bgColor,fontColor;
+    Spinner source,target;
     EditText size;
     SwitchMaterial caption;
     RelativeLayout settings;
-    Button black_font, white_font, grey_font, black_bg, white_bg, grey_bg;
+    Button black_font, white_font, grey_font, black_bg, white_bg, grey_bg, save;
     ImageButton icr_size, dcr_size;
     TextView example;
     float current_size,new_size;
@@ -44,8 +48,22 @@ public class Center extends Fragment {
         size=view.findViewById(R.id.size_edttxt);
         icr_size=view.findViewById(R.id.size_icr);
         dcr_size=view.findViewById(R.id.size_dcr);
+        save=view.findViewById(R.id.save_settings);
 
         example = view.findViewById(R.id.example_caption);
+        size.setText(Float.toString(example.getTextSize()));
+        source=view.findViewById(R.id.source_spinner);
+        target=view.findViewById(R.id.target_spinner);
+        String[] source_list = {"English","Hindi","Arabic", "Catalan", "Welsh", "German", "Estonian", "Persian", "Indonesian", "Japanese", "Latvian", "Mongolian", "Slovenian", "Swedish", "Tamil", "Turkish", "Chinese"};
+        String[] target_list = {"English","Hindi","Arabic", "Catalan", "Welsh", "German", "Estonian", "Persian", "Indonesian", "Japanese", "Latvian", "Mongolian", "Slovenian", "Swedish", "Tamil", "Turkish", "Chinese"};
+        ArrayAdapter<String> source_adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, source_list);
+        ArrayAdapter<String> target_adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, target_list);
+
+        source_adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        target_adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        source.setAdapter(source_adapter);
+        target.setAdapter(target_adapter);
         size.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,13 +73,12 @@ public class Center extends Fragment {
         icr_size.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                increaseTextSize();
             }
         });
         dcr_size.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "dcr", Toast.LENGTH_SHORT).show();
                 decreaseTextSize();
             }
         });
@@ -127,7 +144,10 @@ public class Center extends Fragment {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 settings.setVisibility(b ? View.VISIBLE : View.INVISIBLE);
-                Toast.makeText(getContext(), b ? "Checked" : "Unchecked", Toast.LENGTH_SHORT).show();
+                example.setVisibility(b ? View.VISIBLE : View.INVISIBLE);
+                save.setVisibility(b ? View.VISIBLE : View.INVISIBLE);
+
+
             }
         });
 
@@ -158,13 +178,18 @@ public class Center extends Fragment {
         example.setBackgroundColor(bgColor);
     }
     private void increaseTextSize() {
-        float currentSize = example.getTextSize();
-        example.setTextSize((float) (currentSize + 2.0));
+        current_size = example.getTextSize();
+        new_size=current_size+1;
+        example.setTextSize(TypedValue.COMPLEX_UNIT_PX, new_size);
+        size.setText(Float.toString(new_size));
     }
 
+
     private void decreaseTextSize() {
-        float currentSize = example.getTextSize();
-        example.setTextSize((float) (currentSize - 2.0));
+        current_size = example.getTextSize();
+        new_size=current_size-1;
+        example.setTextSize(TypedValue.COMPLEX_UNIT_PX, new_size);
+        size.setText(Float.toString(new_size));
     }
     private void Resize() {
         String textSizeStr = size.getText().toString();
