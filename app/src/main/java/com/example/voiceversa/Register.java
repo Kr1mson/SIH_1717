5469f1619faaecea3b3e0aed9f6a122f94fbd9b4
Parth Passi <parthpassi@gmail.com>
2024-02-25 20:32:51 +0530

    Login,Registration complete

--- app/src/main/java/com/example/voiceversa/Register.java
+++ app/src/main/java/com/example/voiceversa/Register.java
@@ -1,16 +1,29 @@
 package com.example.voiceversa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity {
     Button signup;
     EditText Name, Email, Password, RePassword;
+
+    FirebaseDatabase userdb;
+    DatabaseReference user_ref;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,55 @@ public class Register extends AppCompatActivity {
         Email=findViewById(R.id.email_signup_edtxt);
         Password=findViewById(R.id.pswd_signup_edtxt);
         RePassword=findViewById(R.id.repswd_edtxt);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=Name.getText().toString();
+                String email=Email.getText().toString();
+                String pswd=Password.getText().toString();
+                String repswd=RePassword.getText().toString();
+                String uniqueUsername = extractUsername(email);
+                if(name.equals("")||email.equals("")||pswd.equals("")) {
+                    Toast.makeText(Register.this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if(pswd.length()>=8) {
+                        if (pswd.equals(repswd)) {
+                            User_Helper uh = new User_Helper(name, email, pswd,uniqueUsername);
+                            userdb = FirebaseDatabase.getInstance("https://voiceversa-76a7b-default-rtdb.firebaseio.com");
+                            user_ref = userdb.getReference("User_Data");
+                            user_ref.child(uniqueUsername).setValue(uh).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Name.setText("");
+                                    Email.setText("");
+                                    Password.setText("");
+                                    RePassword.setText("");
+                                    Toast.makeText(Register.this, "User Registered Successfully", Toast.LENGTH_SHORT).show();
+                                    SharedPreferences preferences_email = getSharedPreferences("user_credentials", MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = preferences_email.edit();
+                                    editor.putString("username", uniqueUsername);
+                                    editor.putString("email", email);
+                                    editor.putString("pswd", pswd);
+                                    editor.apply();
+
+
+                                    Intent i1 = new Intent(getApplicationContext(), Login.class);
+                                    startActivity(i1);
+                                    finish();
+                                }
+                            });
+                        } else {
+                            Toast.makeText(Register.this, "Passwords doesn't match", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(Register.this,"Password Must be atleast 8 characters long",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
     }
     private String extractUsername(String email) {
