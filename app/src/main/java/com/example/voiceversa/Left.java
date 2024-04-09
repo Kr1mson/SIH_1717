812693573a8eee76e155d0268219318f28e1fdd0
Parth Passi <parthpassi@gmail.com>
2024-04-09 11:44:42 +0530

    Added STT for captions 2

--- app/src/main/java/com/example/voiceversa/Left.java
+++ app/src/main/java/com/example/voiceversa/Left.java
@@ -1,32 +1,17 @@
 package com.example.voiceversa;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.speech.RecognitionListener;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
 import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageButton;
 
-import com.google.android.material.switchmaterial.SwitchMaterial;
-
-import java.util.ArrayList;
 
 public class Left extends Fragment {
     public Left(){}
-    private SwitchMaterial micButton;
-    private EditText textField;
-    private SpeechRecognizer speechRecognizer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,88 +21,6 @@ public class Left extends Fragment {
         TransitionInflater tinflater = TransitionInflater.from(requireContext());
         setExitTransition(tinflater.inflateTransition(R.transition.slide_left));
         setEnterTransition(tinflater.inflateTransition(R.transition.slide_left));
-        micButton = view.findViewById(R.id.micButton);
-        textField = view.findViewById(R.id.textField);
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(getContext());
-
-
-        micButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if (isChecked) {
-                    // Setup Speech Recognizer Intent
-                    Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                    speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-
-                    // Start speech recognition and handle results
-                    speechRecognizer.startListening(speechRecognizerIntent);
-                } else {
-                    // Stop speech recognition
-                    speechRecognizer.stopListening();
-                }
-            }
-        });
-
-
-        if (micButton.isChecked()) {
-            // Setup Speech Recognizer Intent
-            Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-
-            // Start speech recognition and handle results
-            speechRecognizer.startListening(speechRecognizerIntent);
-        }
-
-
-        speechRecognizer.setRecognitionListener(new RecognitionListener() {
-            @Override
-            public void onReadyForSpeech(Bundle bundle) {
-
-            }
-
-            @Override
-            public void onBeginningOfSpeech() {
-
-            }
-
-            @Override
-            public void onRmsChanged(float v) {
-
-            }
-
-            @Override
-            public void onBufferReceived(byte[] bytes) {
-
-            }
-
-            @Override
-            public void onEndOfSpeech() {
-
-            }
-
-            @Override
-            public void onError(int i) {
-
-            }
-
-            @Override
-            public void onResults(Bundle results) {
-                ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                if (matches != null && matches.size() > 0) {
-                    textField.setText(matches.get(0));
-                }
-            }
-
-            @Override
-            public void onPartialResults(Bundle bundle) {
-
-            }
-
-            @Override
-            public void onEvent(int i, Bundle bundle) {
-
-            }
-        });
 
         return view;
     }
